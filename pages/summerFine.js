eb70fb060f0ff7d661b2185252dac7bd8cb6eaed
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-02-06 16:07:14 +0100

    nvm

--- pages/summerFine.js
+++ pages/summerFine.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 import { useRouter } from 'next/router'
 
-export default function SummerFine({ allProducts }) {
+export default function summerFine({ allProducts }) {
     const isServerReq = req => !req.url.startsWith('/_next');
     const router = useRouter()
 
