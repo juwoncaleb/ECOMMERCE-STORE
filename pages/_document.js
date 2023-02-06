98808bdc9af428950afeb943da01472309373f6a
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-02-06 02:59:31 +0100

    space

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           
-        <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100;1,100&family=Mukta:wght@200;300;400;500;600;700;800&family=Play:wght@400;700&family=Raleway:ital,wght@0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet"/>        </Head>
+        <link  href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100;1,100&family=Mukta:wght@200;300;400;500;600;700;800&family=Play:wght@400;700&family=Raleway:ital,wght@0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet"/>        </Head>
 
         <body>
 
