424d0182193ade21978f2ee1968d7eb3aa801323
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-02-07 15:18:41 +0100

    two

--- pages/office.js
+++ pages/office.js
@@ -39,7 +39,7 @@ export default function Office ({ offices }) {
 }
 
 export const getServerSideProps = async () => {
-    let prodRes = await fetch("https://lacostestores.vercel.app/api/office")
+    let prodRes = await fetch("https://lacostestores.vercel.app//api/office")
 
     let data = await prodRes.json()
     return {
